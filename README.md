8492e82a32f70f6c2667c00d6216035d180e5d48
RSchueder <rudy.schueder@outlook.com>
2019-08-15 10:36:03 +0200

    Foundations of the web app have been established
    The web app foundations have been established

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # cloud_class
-A deep neural network for classifying cloud types
+A deep neural network for classifying cloud types. The webapp used to host it is in progress.
--- app/app.py
+++ app/app.py
@@ -1,22 +1,60 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
-my_app = Flask('flask_demo')
+# FLASK_APP=flaskblog.py
+# FLASK_DEBUG=1
 
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '2dfb5258eda9b125e3252bfc43c71ed1'
+
+posts = [
+	{'author' : 'Rudy',
+	 'title' : 'blog post 1',
+	 'content' : 'First post content',
+	 'date_posted' : 'April 20, 2018'},
+
+
+	{'author' : 'Jane',
+	 'title' : 'my trip',
+	 'content' : 'info about my trip',
+	 'date_posted' : 'June 20, 2017'},
+]
+
+# home page
+# two routes handled by the same function
 @app.route('/')
-def show_predict_form():
-    return render_template('predictorform.html')
+@app.route('/home')
+def home():
+	return render_template('home.html', posts = posts)
 
-# this line is decorating the results function
-@app.route('/results', methods = ['POST'])
+@app.route('/about')
+def about():
+	return render_template('about.html', title = 'About')
 
-def results():
-    form = request.form
-    if request.method == 'POST':
-        # write the function that loads the model
-        model = get_model()
-        year = request.form['year']
-        predicted_val = model.predict(year)
-        return render_template('resultsform.html', year = year, predicted_val = predicted_val)
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		# flash the messages, flashed messages are picked up in layout, and displayed
+		flash(f'Account created for {form.username.data}!', 'success')
+		return redirect(url_for('home'))
 
+	return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+	else:
+		flash('Login Unsuccessful. Please check username and password', 'danger')
+	return render_template('login.html', title='Login', form=form)
+
+
+
+# true if we run this script directly
 if __name__ == '__main__':
-    my_app.run('localhost', '9999', debug = True)
+	app.run(debug=True)
